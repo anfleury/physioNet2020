bacb1cc07b027f3f1292ddfc897747f6da117509
Amanda Fleury <afleury01@gmail.com>
2020-05-06 17:14:29 -0400

    First commit -- physioNet example code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data set and analysis for the 2018 physioNet challenge.',
+    author='amanda',
+    license='MIT',
+)
